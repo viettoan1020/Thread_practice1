f35e69574960e63ad1dd5bf3cf89657a96d6befb
viettoan1020 <viettoann102003@gmail.com>
2024-01-02 16:21:01 +0700

    thuchanh1

---
+++ Runnable/src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+            System.out.println("Main thread running...");
+
+            RunnableDemo runnableDemo1 = new RunnableDemo("Thread-1-HR-Database");
+            runnableDemo1.start();
+            RunnableDemo runnableDemo2 = new RunnableDemo("Thread-2-Send-Email");
+            runnableDemo2.start();
+        System.out.println("Main thread stopped!!!");
+    }
+}
